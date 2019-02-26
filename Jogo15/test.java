d2dba9cacae091df80a54a00863696993b800980
Luis Trinta <33879012+luistrinta@users.noreply.github.com>
2019-02-26 18:28:51 +0000

    Update test.java

--- Jogo15/test.java
+++ Jogo15/test.java
@@ -1,28 +1,33 @@
 import java.util.*;
-  public class test extends Aux{
+public class test extends Aux {
 
     public static Scanner scan = new Scanner(System.in);
 
-public static void main(String[] args) {
- //BFS test
- int[] t_arr = new int[16];
- int[] t2_arr = new int[16];
+    public static void main(String[] args) {
+        //BFS test
+        int[] t_arr = new int[16];
+        int[] t2_arr = new int[16];
 
-  Tabela t_inicial = new Tabela(t_arr);
-  Tabela t_final =new Tabela(t2_arr);
+        Tabela t_inicial = new Tabela(t_arr);
+        Tabela t_final = new Tabela(t2_arr);
 
-  scanTabela(t_inicial);
-  scanTabela(t_final);
-boolean init = testSolvability(t_inicial);
-boolean fin = testSolvability(t_final);
-if(init && fin){
-BFS(t_inicial,t_final);
-}
-else{
-  if(!init)
-  System.out.println("Este tabuleiro é impossivel");
+        scanTabela(t_inicial);
+        scanTabela(t_final);
+        boolean init = testSolvability(t_inicial);
+        boolean fin = testSolvability(t_final);
+        if (init && fin) {
 
-  else System.out.println("Esta solução é impossível");
-}
-}
+            //BFS(t_inicial, t_final);
+            System.out.println("Insert depth");
+            Scanner scan = new Scanner(System.in);
+
+            DFS(t_inicial, t_final, scan.nextInt());
+
+        } else {
+            if (!init)
+                System.out.println("Este tabuleiro é impossivel");
+
+            else System.out.println("Esta solução é impossível");
+        }
+    }
 }
