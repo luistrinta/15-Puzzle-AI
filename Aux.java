a988b8bcd6b7d508fc865a7f273e61f1ef387659
Luis Trinta <33879012+luistrinta@users.noreply.github.com>
2019-03-01 17:16:07 +0000

    Update Aux.java

--- Aux.java
+++ Aux.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Aux extends Tabela {
 
-    static void BFS(Tabela t_inicial, Tabela t_final) {
+    static void BFS(Tabela t_inicial, int[] t_final) {
         long start_Time = System.currentTimeMillis();
         long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
@@ -10,11 +10,14 @@ public class Aux extends Tabela {
         HashMap < String, Tabela > map = new HashMap < String, Tabela > ();
 
         int counter = 0;
+        // int filhos =0;
         queue.add(t_inicial);
         boolean flag = true;
         while (flag) {
 
             Tabela valor = queue.poll();
+            //filhos ++;
+
             String key = Arrays.toString(getArr(valor));
             if (!map.containsKey(key)) {
                 map.put(key, valor);
@@ -33,17 +36,18 @@ public class Aux extends Tabela {
             }
 
 
-            if (equalsTabela(valor.arr, t_final.arr)) {
+            if (equalsTabela(valor.arr, t_final)) {
                 flag = false;
                 long stop_Time = System.currentTimeMillis();
                 long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
                 long actualMemUsed = afterUsedMem - beforeUsedMem;
                 long tempo_exec = (stop_Time - start_Time);
                 System.out.println("Execution time: " + tempo_exec + "ms");
-                System.out.println("Memory used : " + actualMemUsed + " Bytes");
+                System.out.println("Memory used : " + actualMemUsed / 1000000 + "MB");
                 System.out.println("Path:" + valor.path);
                 System.out.println("Path length:" + valor.path.length());
-                System.out.println(" nº Tabelas : " + counter);
+                System.out.println(" nº nos : " + counter);
+                // System.out.println("nº visitados :" + filhos);
             }
         }
     }
@@ -86,7 +90,7 @@ public class Aux extends Tabela {
         }
     }
 
-    static void DFS(Tabela t_inicial, Tabela t_final, int depth) {
+    static void DFS(Tabela t_inicial, int[] t_final) {
         long start_Time = System.currentTimeMillis();
         long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
@@ -100,7 +104,7 @@ public class Aux extends Tabela {
 
             Tabela valor = queue.pop();
             String key = Arrays.toString(getArr(valor));
-            if(valor.path.length() <= depth) {
+            if(valor.path.length() <= 80) {
                 if (!map.containsKey(key)) {
                     map.put(key, valor);
 
@@ -119,33 +123,35 @@ public class Aux extends Tabela {
             }
 
 
-            if (equalsTabela(valor.arr, t_final.arr)) {
+            if (equalsTabela(valor.arr, t_final)) {
 
                 long stop_Time = System.currentTimeMillis();
                 long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
                 long actualMemUsed = afterUsedMem - beforeUsedMem;
                 long tempo_exec = (stop_Time - start_Time);
                 System.out.println("Execution time: " + tempo_exec + "ms");
-                System.out.println("Memory used : " + actualMemUsed + " Bytes");
+                System.out.println("Memory used : " + actualMemUsed / 1000000 + "MB");
                 System.out.println("Path:" + valor.path);
                 System.out.println("Path length:" + valor.path.length());
-                System.out.println(" nº Tabelas : " + counter);
+                System.out.println(" nº nos : " + counter);
                 return;
             }
         }
         System.out.println("Não existe solução a esta profundidade");
     }
+
+
     static String[] auxDFS(Tabela t_inicial, Tabela t_final, int depth) {
         Stack < Tabela > queue = new Stack < > ();
         HashMap < String, Tabela > map = new HashMap < String, Tabela > ();
         int counter = 0;
         queue.push(t_inicial);
-        String[] v={"1","","0"}; 
+        String[] v = {"1", "", "0"};
         while (!queue.isEmpty()) {
             Tabela valor = queue.pop();
             String key = Arrays.toString(getArr(valor));
             if(valor.path.length() <= depth) {
-                if (!map.containsKey(key)) {
+               if (!map.containsKey(key)) {
                     map.put(key, valor);
                     String[] s = checkMoves(valor);
                     for (String str : s) {
@@ -159,23 +165,23 @@ public class Aux extends Tabela {
                 }
             }
             if (equalsTabela(valor.arr, t_final.arr)) {
-                v[2]=Integer.toString(counter);
-                v[1]=valor.path;
+                v[2] = Integer.toString(counter);
+                v[1] = valor.path;
                 return v;
             }
         }
-        v[2]=Integer.toString(counter);
-        v[0]="0";
+        v[2] = Integer.toString(counter);
+        v[0] = "0";
         return v;
     }
     public static void IDFS(Tabela t_inicial, Tabela t_final) {
         long start_Time = System.currentTimeMillis();
         long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        int counter=1,i=0,fours=1,f1=1;
-        String[] v= auxDFS(t_inicial, t_final, 0);
-        while(v[0]=="0") {
-            v=auxDFS(t_inicial, t_final, i);
-            counter=counter+Integer.parseInt(v[2]);
+        int counter = 1, i = 0, fours = 1, f1 = 1;
+        String[] v = auxDFS(t_inicial, t_final, 0);
+        while(v[0] == "0") {
+            v = auxDFS(t_inicial, t_final, i);
+            counter = counter + Integer.parseInt(v[2]);
             i++;
         }
         long stop_Time = System.currentTimeMillis();
@@ -190,7 +196,241 @@ public class Aux extends Tabela {
         return;
     }
 
+    //Heuristicas 
+
+    public static int heuristicSum(int[] t_avaliar, int[] t_final) {
+
+        int counter = 0;
+        for(int i = 0; i < 16; i++)
+            if(t_avaliar[i] != t_final[i])
+                counter++;
+        return counter;
+    }
+    public static int heuristicManhattan(int[] t_avaliar, int[] t_final) {
+
+        int counter = 0;
+        int pos = 0, j;
+        for(int i = 0; i < 16; i++) {
+            for(j = 0; j < 16; j++) {
+                if(t_avaliar[i] == t_final[j]) {
+                    pos = j;
+                    break;
+                }
+            }
+            int ii = i + 1;
+            int poss = pos + 1;
+            int pos1[] = {(ii % 4 != 0) ? ii / 4 + 1 : ii / 4, ii % 4 == 0 ? 4 : ii % 4};
+            int pos2[] = {(poss % 4 != 0) ? poss / 4 + 1 : poss / 4, poss % 4 == 0 ? 4 : poss % 4};
+            //System.out.printf("Numero %02d | Posição Array %02d | Coordenadas1 %02dx%02dy | Coordenadas2 %02dx%02dy\n", arr[i], poss, pos1[0], pos1[1], pos2[0], pos2[1]);
+            //System.out.println(counter+" "+(pos1[0]>=pos2[0])+" "+ (pos1[0]>=pos2[0] ? pos1[0]-pos2[0] : pos2[0]-pos1[0])+"+"+(pos1[1]>=pos2[1] ? pos1[1]-pos2[1] : pos2[1]-pos1[1]));
+            counter = counter + (pos1[0] >= pos2[0] ? pos1[0] - pos2[0] : pos2[0] - pos1[0])
+                      + (pos1[1] >= pos2[1] ? pos1[1] - pos2[1] : pos2[1] - pos1[1]);
+        }
+        return counter;
+    }
+
+    //----------------------------
+
+    //Heuristicas utilizadas : Sum of Different Tiles e Manhattan Distance
+
+    static class SumComparator implements Comparator<Tabela> {
+
+        //Override do metodo compare do Comparator
+        public int compare(Tabela t1, Tabela t2) {
+            if (heuristicSum(t1.arr, t1.solution) > heuristicSum(t2.arr, t2.solution))
+                return 1;
+            else if (heuristicSum(t1.arr, t1.solution) < heuristicSum(t2.arr, t2.solution))
+                return -1;
+            return 0;
+        }
+    }
+
+    static class ManhattanComparator implements Comparator<Tabela> {
+
+        //Override do metodo compare do Comparator
+        public int compare(Tabela t1, Tabela t2) {
+            if (heuristicManhattan(t1.arr, t1.solution) > heuristicManhattan(t2.arr, t2.solution))
+                return 1;
+            else if (heuristicManhattan(t1.arr, t1.solution) < heuristicManhattan(t2.arr, t2.solution))
+                return -1;
+            return 0;
+        }
+    }
+    
+    static class A_starComparator implements Comparator<Tabela> {
+
+        //Override do metodo compare do Comparator
+        public int compare(Tabela t1, Tabela t2) {
+            if (heuristicManhattan(t1.arr, t1.solution) > heuristicManhattan(t2.arr, t2.solution)){
+                if (heuristicSum(t1.arr, t1.solution) > heuristicSum(t2.arr, t2.solution))
+                    return 1;
+                
+                    return -1;
+            }
+                return -1;
+
+        }
+    }
+
+ 
+
+
+
+    public static void greedy_Alg_Manhattan(Tabela t_inicial, int[] t_final ) {
+
+        long start_Time = System.currentTimeMillis();
+        long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        PriorityQueue < Tabela > queue = new PriorityQueue <Tabela>(new ManhattanComparator());
+        HashMap < String, Tabela > map = new HashMap < String, Tabela > ();
+
+        int counter = 0;
+        // int filhos =0;
+        queue.add(t_inicial);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+
+            Tabela valor = queue.poll();
+            //filhos ++;
+
+            String key = Arrays.toString(getArr(valor));
+            if (!map.containsKey(key)) {
+                map.put(key, valor);
+
+                String[] s = checkMoves(valor);
+
+                for (String str : s) {
+                    if (str != null) {
+                        Tabela t2 = moveTabela(str, valor);
+                        if (!(Arrays.equals(getPai(t2), getPai(valor))))
+                            queue.add(t2);
+                        counter++;
+                    }
+                }
+
+            }
+
+
+            if (equalsTabela(valor.arr, t_final)) {
+                long stop_Time = System.currentTimeMillis();
+                long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                long actualMemUsed = afterUsedMem - beforeUsedMem;
+                long tempo_exec = (stop_Time - start_Time);
+                System.out.println("Execution time: " + tempo_exec + "ms");
+                System.out.println("Memory used : " + actualMemUsed / 1000000 + "MB");
+                System.out.println("Path:" + valor.path);
+                System.out.println("Path length:" + valor.path.length());
+                System.out.println(" nº nos : " + counter);
+                return;
+            }
+        }
+    }
+
+
+
+    public static void greedy_Alg_TileSum(Tabela t_inicial, int[] t_final) {
+
+        long start_Time = System.currentTimeMillis();
+        long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        PriorityQueue < Tabela > queue = new PriorityQueue <Tabela>(new SumComparator());
+        HashMap < String, Tabela > map = new HashMap < String, Tabela > ();
+
+        int counter = 0;
+        // int filhos =0;
+        queue.add(t_inicial);
+        
+        while (!queue.isEmpty()) {
+
+            Tabela valor = queue.poll();
+            //filhos ++;
+
+            String key = Arrays.toString(getArr(valor));
+            if (!map.containsKey(key)) {
+                map.put(key, valor);
+
+                String[] s = checkMoves(valor);
+
+                for (String str : s) {
+                    if (str != null) {
+                        Tabela t2 = moveTabela(str, valor);
+                        if (!(Arrays.equals(getPai(t2), getPai(valor))))
+                            queue.add(t2);
+                        counter++;
+                    }
+                }
+
+            }
+
+
+            if (equalsTabela(valor.arr, t_final)) {
+                long stop_Time = System.currentTimeMillis();
+                long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                long actualMemUsed = afterUsedMem - beforeUsedMem;
+                long tempo_exec = (stop_Time - start_Time);
+                System.out.println("Execution time: " + tempo_exec + "ms");
+                System.out.println("Memory used : " + actualMemUsed / 1000000 + "MB");
+                System.out.println("Path:" + valor.path);
+                System.out.println("Path length:" + valor.path.length());
+                System.out.println(" nº nos : " + counter);
+                return;
+            }
+        }
+    }
+
+
+
+    public static void a_star(Tabela t_inicial, int[] t_final ) {
+
+        long start_Time = System.currentTimeMillis();
+        long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        PriorityQueue < Tabela > queue = new PriorityQueue <Tabela>(new A_starComparator());
+        HashMap < String, Tabela > map = new HashMap < String, Tabela > ();
+
+        int counter = 0;
+        // int filhos =0;
+        queue.add(t_inicial);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+
+            Tabela valor = queue.poll();
+            //filhos ++;
+
+            String key = Arrays.toString(getArr(valor));
+            if (!map.containsKey(key)) {
+                map.put(key, valor);
+
+                String[] s = checkMoves(valor);
+
+                for (String str : s) {
+                    if (str != null) {
+                        Tabela t2 = moveTabela(str, valor);
+                        if (!(Arrays.equals(getPai(t2), getPai(valor))))
+                            queue.add(t2);
+                        counter++;
+                    }
+                }
+
+            }
+
+
+            if (equalsTabela(valor.arr, t_final)) {
+                long stop_Time = System.currentTimeMillis();
+                long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                long actualMemUsed = afterUsedMem - beforeUsedMem;
+                long tempo_exec = (stop_Time - start_Time);
+                System.out.println("Execution time: " + tempo_exec + "ms");
+                System.out.println("Memory used : " + actualMemUsed / 1000000 + "MB");
+                System.out.println("Path:" + valor.path);
+                System.out.println("Path length:" + valor.path.length());
+                System.out.println(" nº nos : " + counter);
+                return;
+            }
+        }
+    }
+
+
 }
+
 
 
 
