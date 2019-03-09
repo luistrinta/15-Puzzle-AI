6bfa98d095de4abcb5d89521b541a7e906a3766e
Luis Trinta <33879012+luistrinta@users.noreply.github.com>
2019-03-09 15:57:10 +0000

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class test extends Aux {
+public class J15 extends Aux {
 
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-       
+        //BFS test
         int[] t_arr = new int[16];
         int[] t2_arr = new int[16];
 
@@ -18,43 +18,56 @@ public class test extends Aux {
         if (init && fin) {
             System.out.print("\033[H\033[2J");
             //Runtime.getRuntime().exec("cls");
-            System.out.printf("###################\n1.BFS\n2.DFS\n3.IDFS\n4.Greedy\n5.A*\n6.All\n\nOption:");
+            System.out.printf("###################\n1.BFS\n2.DFS\n3.IDFS\n4.Greedy\n5.A*\n6.All\n\n");
             Scanner scan = new Scanner(System.in);
-            int option = scan.nextInt();
-            //int option=6;
-            switch ((int)option) {
+            boolean x = true;
+            while(x){
+            System.out.println("Option :");
+            String option = scan.nextLine();
+           
+            switch (option) {
 
-            case 1:
+            case "1":
                 System.out.printf("\n###################\n\n|__BFS__|\n");
                 BFS(t_inicial, t_inicial.solution);
+                x = false;
                 break;
 
-            case 2:
+            case "2":
                 System.out.printf("\n###################\n\n|__DFS__|\n");
                 System.out.println("\nProfundidade utilizada : 80");
                 System.out.println("\n##################\n\n");
                 DFS(t_inicial, t_final.arr);
+                x = false;
                 break;
 
-            case 3:
+            case "3":
                 System.out.printf("\n###################\n\n|__IDFS__|\n");
                 IDFS(t_inicial, t_final);
+                x = false;
                 break;
 
-            case 4:
+            case "4":
                 System.out.printf("\n###################\n\n|__Greedy__|\n");
                 System.out.printf("###################\n1.Manhattan Distance\n2.Tile Sum\n");
                 int option2 = scan.nextInt();
                 if(option2 == 1)
                     greedy_Alg_Manhattan(t_inicial, t_inicial.solution);
                 else greedy_Alg_TileSum(t_inicial, t_inicial.solution);
+                x = false;
                 break;
 
-            case 5:
+            case "5":
                 System.out.printf("\n###################\n\n|__A*__|\n");
-                a_star(t_inicial, t_inicial.solution);
+                System.out.printf("###################\n1.Manhattan Distance\n2.Tile Sum\n");
+                option2 = scan.nextInt();
+                if(option2== 1)
+                a_star_M(t_inicial, t_inicial.solution);
+            else 
+                a_star_TS(t_inicial,t_inicial.solution);
+                x = false;
                 break;
-            case 6:
+            case "6":
                 System.out.printf("\n###################\n\n|__BFS__|\n");
                 BFS(t_inicial, t_inicial.solution);
                 System.out.printf("\n###################\n\n|__DFS__|\n");
@@ -65,10 +78,17 @@ public class test extends Aux {
                 greedy_Alg_Manhattan(t_inicial, t_inicial.solution);
                 System.out.printf("\n###################\n\n|__Greedy_Alg_Sum_Tiles__|\n");
                 greedy_Alg_TileSum(t_inicial, t_inicial.solution);
-                System.out.printf("\n###################\n\n|__A*__|\n");
-                a_star(t_inicial, t_inicial.solution);
+                System.out.printf("\n###################\n\n|__A*_Manhattan_|\n");
+                a_star_M(t_inicial, t_inicial.solution);
+                System.out.printf("\n###################\n\n|__A*_Tile_Sum_|\n");
+                a_star_TS(t_inicial , t_inicial.solution);
+                x = false;
+                break;
+            default:
+            System.out.println("Por Favor insira uma opção válida");
+            break;
             }
-
+        }
 
 
 
